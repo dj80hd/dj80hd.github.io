c6f59d225b9dca76f3dfd1ee4acd48f93c8a211c
dj80hd <werwath@gmail.com>
2015-05-05 13:15:38 +0000

    expand dpa

--- alias.sh
+++ alias.sh
@@ -4,9 +4,20 @@ function d() { sudo docker "$@" ;}
 function de() { sudo docker exec -t "$@" ;}
 function di() { sudo docker exec -i -t $1 bash ;}
 function dr() { sudo docker rm $1 ;}
-function dpa() { sudo docker ps -a ;}
+function dpa() { 
+    if [ -z "$1" ]; then
+        sudo docker ps -a 
+    else
+        sudo docker ps -a |grep $1
+    fi
+}
 function dp() { sudo docker ps ;}
 function dl() { sudo docker logs $1 ;}
+function ds() {
+    for p in `sudo docker ps|grep $1 |cut -d' ' -f 1` ; do 
+        udo docker stop $p 
+    done
+}
 
 function dip() { 
     docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1 
