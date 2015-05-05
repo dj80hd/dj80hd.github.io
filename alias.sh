112ba22898899e6714724241ec5a5adcff3ecac6
dj80hd <werwath@gmail.com>
2015-05-05 00:02:42 +0000

    dip

--- alias.sh
+++ alias.sh
@@ -1,6 +1,18 @@
-#my aliases
+
+#docker functions
 function d() { sudo docker "$@" ;}
+function dpa() { sudo docker ps -a ;}
+function dp() { sudo docker ps ;}
+
+function dip() { 
+    docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1 
+}
+
+
+
+#my aliases
 alias copy='cp'
 alias h='history'
 
+#my vars
 LH=http://127.0.0.1
