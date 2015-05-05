2c39abefce9e36489fc2db7ff47af2c5784c20e7
dj80hd <werwath@gmail.com>
2015-05-05 12:51:54 +0000

    di de aliases

--- alias.sh
+++ alias.sh
@@ -1,6 +1,8 @@
 
 #docker functions
 function d() { sudo docker "$@" ;}
+function de() { sudo docker exec -t "$@" ;}
+function di() { sudo docker exec -i -t $1 bash ;}
 function dr() { sudo docker rm $1 ;}
 function dpa() { sudo docker ps -a ;}
 function dp() { sudo docker ps ;}
