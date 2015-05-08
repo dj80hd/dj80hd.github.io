8b4a3d57143ff3fc7020130778774582ce559764
dj80hd <werwath@gmail.com>
2015-05-08 13:22:13 +0000

    add dprm

--- alias.sh
+++ alias.sh
@@ -4,6 +4,8 @@ function d() { sudo docker "$@" ;}
 function de() { sudo docker exec -t "$@" ;}
 function di() { sudo docker exec -i -t $1 bash ;}
 function dr() { sudo docker rm $1 ;}
+
+#Show all docker process, optional input match string
 function dpa() { 
     if [ -z "$1" ]; then
         sudo docker ps -a 
@@ -11,11 +13,22 @@ function dpa() {
         sudo docker ps -a |grep $1
     fi
 }
+
 function dp() { sudo docker ps ;}
 function dl() { sudo docker logs $1 ;}
-function ds() {
+
+#Stop any containers matching input
+function dps() {
     for p in `sudo docker ps|grep $1 |cut -d' ' -f 1` ; do 
-        udo docker stop $p 
+        sudo docker stop $p 
+    done
+}
+
+#Stop and remove any containers matching input
+function dprm() {
+    for p in `sudo docker ps -a|grep $1 |cut -d' ' -f 1` ; do 
+        sudo docker stop $p 
+        sudo docker rm $p 
     done
 }
 
