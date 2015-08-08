b25e2601a718c6d21f6d26ee968e28a903719cd6
SHIEncrypt <shiencrypt@TUSPA0BOMLVT018.local>
2015-08-08 14:50:35 -0500

    install_java

--- alias.sh
+++ alias.sh
@@ -488,10 +488,17 @@ function vztun() {
         
 
 }
+function install_java() {
+    sudo add-apt-repository ppa:webupd8team/java -y
+    sudo apt-get update
+    sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+    sudo apt-get install oracle-java7-installer -y --force-yes
+}
 
 # Env vars
 function myenv() {
   export VS=~/home/boxes/vz/scripts
   export VZG=ssh://git@aloha.icsl.net:2223/aloha
 }
+
 
