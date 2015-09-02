d96d8c90d9d51be4fc5d446f5ab71bc0c5e0ffdc
Jimi Werwath <werwath@gmail.com>
2015-09-02 12:17:33 -0500

    gs

--- alias.sh
+++ alias.sh
@@ -235,6 +235,9 @@ function blamealls() {
     #trims leading/tailing whitespace
     blameall |perl -e 'while(<>){ print "$1\n" if /\(([\w ]+)\s+20/;}'|sort -n |awk '{$1=$1};1'|uniq -c
 }
+function gs() {
+    git status
+}
 
 #- Convenience method to use git log
 function gln() {
