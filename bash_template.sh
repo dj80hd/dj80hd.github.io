abe0f34f8d0bbd02901461b307b1c4b34073d024
Jimi Werwath <jim.werwath@uptake.com>
2018-11-21 11:21:55 -0600

    cleanup

--- bash_template.sh
+++
@@ -1,8 +0,0 @@
-set -e
-__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__FILE="${__DIR}/$(basename "${BASH_SOURCE[0]}")"
-__BASE="$(basename ${__FILE} .sh)"
-__ROOT="$(cd "$(dirname "${__DIR}")" && pwd)"
-
-
-ARG1="${1:-}"
